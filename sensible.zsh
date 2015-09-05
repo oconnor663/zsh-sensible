3024bef3104264061e7331cd4c34e0c84723364b
Jack O'Connor <oconnor663@gmail.com>
2015-09-05 16:51:50 -0400

    get rid of the extended globbing header
    The more I look at this, the more it seems too niche to enable by
    default. The downside is that you have to be careful with characters
    like '^'.

--- sensible.zsh
+++ sensible.zsh
@@ -8,7 +8,6 @@ setopt hist_ignore_dups
 setopt hist_ignore_space
 
 
-# ------------------- extended globbing --------------------------------
 # ------------------- enable completion --------------------------------
 # ------------------- command line $EDITOR --------------------------------
 
